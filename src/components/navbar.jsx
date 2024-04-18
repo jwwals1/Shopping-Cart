7fd482b9274261a712bc5ca4e87881c52640c839
jwwals1 <joe_walsh59@yahoo.com>
2024-04-17 19:55:47 -0500

    added navbar component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+function navbar() {
+    return ( 
+        <>
+        <div>
+            <div>
+                <h2>Homepage</h2>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default navbar
